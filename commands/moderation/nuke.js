bb7704e817dc8ae043b8f6a424beb827f4f5c60a
Smittt <shindesmit314@gmail.com>
2025-07-22 23:41:00 +0530

    Update nuke.js

--- commands/moderation/nuke.js
+++ commands/moderation/nuke.js
@@ -3,7 +3,7 @@ const {
   EmbedBuilder,
   AttachmentBuilder,
 } = require('discord.js');
-const { path } = require('path');
+const path = require('path');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,35 +14,37 @@ module.exports = {
   async execute(interaction) {
     if (!interaction.member.permissions.has('ManageChannels')) {
       return interaction.reply({
-        content: 'You do not have `ManageChannels` permission to nuke members!',
+        content: 'You do not have `ManageChannels` permission!',
         ephemeral: true,
       });
     }
     const channelToNuke = interaction.channel;
 
     if (channelToNuke.type !== 0) {
-      return interaction.reply(
-        'This command can only be used in text channels.'
-      );
+      // 0 represents a GUILD_TEXT channel
+      return interaction.reply({
+        content: 'This command can only be used in text channels.',
+        ephemeral: true,
+      });
     }
 
     try {
-      const channelName = channelToNuke.name;
       const channelPosition = channelToNuke.position;
-
       const newChannel = await channelToNuke.clone({
-        name: channelName,
         position: channelPosition,
-        reason: 'Channel nuked by command',
+        reason: `Channel nuked by ${interaction.user.tag}`,
       });
 
-      await channelToNuke.delete('Channel nuked by command');
-      const attachment = new AttachmentBuilder('./utils/nuke.gif');
+      await channelToNuke.delete(`Nuked by ${interaction.user.tag}`);
+
+      const attachment = new AttachmentBuilder(
+        path.join(__dirname, '../../utils/nuke.gif')
+      ); // Using path.join for better file path handling
       const embed = new EmbedBuilder()
         .setColor('#FF0000')
         .setTitle('💣 Channel Nuked! 💣')
         .setImage('attachment://nuke.gif')
-        .setFooter({ text: `Nuked by ${interaction.user.tag}.` })
+        .setFooter({ text: `Nuked by ${interaction.user.tag}` })
         .setTimestamp();
 
       const nukeMessage = await newChannel.send({
@@ -50,14 +52,26 @@ module.exports = {
         files: [attachment],
       });
 
-setTimeout(async () => {
-  try {
-    await nukeMessage.delete();
-  } catch (error) {
-    // Ignores the error if the message is already deleted (Error Code 10008)
-    // but logs any other unexpected errors.
-    if (error.code !== 10008) {
-      console.error('Failed to delete the nuke confirmation message:', error);
+      // Deletes the "Channel Nuked!" message after 30 seconds
+      setTimeout(async () => {
+        try {
+          await nukeMessage.delete();
+        } catch (error) {
+          // Ignores the error if the message is already deleted (Error Code 10008)
+          if (error.code !== 10008) {
+            console.error(
+              'Failed to delete the nuke confirmation message:',
+              error
+            );
+          }
+        }
+      }, 30000);
+    } catch (error) {
+      console.error('Error during nuke operation:', error);
+      await interaction.reply({
+        content: 'There was an error trying to nuke this channel.',
+        ephemeral: true,
+      });
     }
-  }
-}, 30000);
+  },
+};
